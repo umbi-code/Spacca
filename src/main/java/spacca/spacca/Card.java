69451493f3c9da03c4e78dd95f0a58c95c903f36
tesfaye174 <tesfaye.venieri@studio.unibo.it>
2024-04-19 14:53:47 +0200

    progetto tes

---
+++ src/main/java/spacca/spacca/Card.java
@@ -0,0 +1,43 @@
+/*package spacca.spacca;
+
+import java.util.List;
+
+public class Card{
+    private String faceNome, suit;
+    // private Image image;
+
+    public Card(String faceNome, String suit){
+        SetFaceNome(faceNome);
+        SetSuit(suit);
+    }
+
+    public String getFaceNome() {
+        return faceNome;
+    }
+    /**
+     qui bisogna specificare le tipologie di carte
+     * @param faceNome
+     **/
+    /*public static List<String> getValidFaceNomes(){
+        List<String> faceNomes = new ArrayList<String>();
+    }
+
+
+    public void setFaceNome(String faceNome) {
+        this.faceNome = faceNome;
+    }
+
+    private void SetSuit(String suit) {
+    }
+
+    private void SetFaceNome(String faceNome) {
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+}*/
